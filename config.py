dcdd142b5ea07f01ad4c3a8f9b5bd021fb0f6f49
nikantropov <nikolai.antropov@gmail.com>
2026-07-07 19:49:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 ADMIN_IDS = [int(x.strip()) for x in os.environ.get("ADMIN_IDS", "1214258573,472343594").split(",") if x.strip()]
 
-DB_PATH = os.environ.get("DB_PATH", "./data/vivino_bot.db")
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
 
 SCREENSHOTS_DIR = os.environ.get("SCREENSHOTS_DIR", "./data/screenshots")
 
